86e2b5868bf14e5fe6841fcccc40c0e50824048d
Milko Daskalov <mdaskalov@outlook.com>
2017-03-05 12:04:16 +0100

    Initial Commit

---
+++ Maze/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Maze
+//
+//  Created by Mike Daskaloff on 05.03.17.
+//  Copyright © 2017 Milko Daskalov. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Maze/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by Mike Daskaloff on 05.03.17.
+//  Copyright © 2017 Milko Daskalov. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
