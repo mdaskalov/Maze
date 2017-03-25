ac64a7c108505673a75e6b7ebc22e17679f06597
Milko Daskalov <mdaskalov@outlook.com>
2017-03-25 13:35:01 +0100

    Improved pan using gesture recognition under macOS

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    private let boxSize = 3
+    private let boxSize = 2
     private let boxMapWidth = 41 //41
     private let boxMapHeight = 31 //31
     private let initialZoom: CGFloat = 21.0 // 21.0
@@ -76,31 +76,59 @@ class GameScene: SKScene {
         }
     }
     
-    func touchDown(atPoint: CGPoint) {
-        touchPos=atPoint
+    func tap(at: CGPoint) {
+        //print("tap")
         
-        if let maze = self.maze, let mazeSolver = self.mazeSolver {
-            if let clickBox = maze.tileBox(fromPosition: self.convertPoint(fromView: atPoint)) {
-                mazeSolver.setPoint(at: clickBox)
-                print(String(format: "click at: %03d,%03d", clickBox.x, clickBox.y))
-            }
+        if let maze = self.maze, let mazeSolver = self.mazeSolver, let clickBox = maze.tileBox(fromPosition: self.convertPoint(fromView: at)) {
+            //print(String(format: "click at: %03d,%03d", clickBox.x, clickBox.y))
+            mazeSolver.setPoint(at: clickBox)
         }
-        //print(String(format: "  began Pan loc: %4.2f,%4.2f", self.touchPos!.x,self.touchPos!.y))
     }
     
-    func touchMoved(toPoint: CGPoint) {
+    func doubleTap() {
+        //print("doubleTap")
+        
+        if let maze = self.maze, let mazeSolver = self.mazeSolver {
+            mazeSolver.solveMaze(start: maze.randomTileBox(), end: maze.randomTileBox(), animateCamera: true)
+        }
+    }
+    
+    func longPress() {
+        mazeSolver?.abortSolving()
+        resetCut()
+        //maze?.cutMaze()
+    }
+    
+    func panBegan(location: CGPoint, translation: CGPoint, velocity: CGPoint) {
+        touchPos=location
+
+        //print(String(format: "  began Pan loc: %4.2f,%4.2f tra: %4.2f,%4.2f vel: %4.2f,%4.2f", location.x,location.y,translation.x,translation.y,velocity.x,velocity.y))
+    }
+    
+    func panChanged(location: CGPoint, translation: CGPoint, velocity: CGPoint) {
+        
         if let camera = self.camera, let touchPos = self.touchPos {
-            let translation = CGPoint(x: touchPos.x - toPoint.x, y: touchPos.y - toPoint.y)
+            let translation = CGPoint(x: touchPos.x - location.x, y: touchPos.y - location.y)
             
             camera.position.x += (translation.x * (camera.xScale))
             camera.position.y += (translation.y * (camera.yScale))
             
-            self.touchPos = toPoint
-            //print(String(format: "changed Pan loc: %4.2f,%4.2f", self.touchPos!.x,self.touchPos!.y))
+            self.touchPos = location
         }
+
+        /*
+        camera!.position.x += (translation.x * (camera!.xScale / 10))
+        camera!.position.y += (translation.y * (camera!.yScale / 10))
+        */
+        //print(String(format: "changed Pan loc: %4.2f,%4.2f tra: %4.2f,%4.2f vel: %4.2f,%4.2f", location.x,location.y,translation.x,translation.y,velocity.x,velocity.y))
     }
     
-    func touchUp(atPoint pos : CGPoint) {
+    func panEnded(location: CGPoint, translation: CGPoint, velocity: CGPoint) {
+        if abs(translation.x) < 0.5 && abs(translation.y) < 0.5 {
+            tap(at: location)
+            return
+        }
+        //print(String(format: "  ended Pan loc: %4.2f,%4.2f tra: %4.2f,%4.2f vel: %4.2f,%4.2f", location.x,location.y,translation.x,translation.y,velocity.x,velocity.y))
     }
     
     func zoom(delta: CGFloat) {
@@ -109,24 +137,12 @@ class GameScene: SKScene {
             if scale < 0.8 {
                 scale = 0.8
             }
-            if scale > 25 {
-                scale = 25
+            if scale > initialZoom {
+                scale = initialZoom
             }
             camera.setScale(scale)
             //print(String(format: "zoom: %.2f", camera.xScale))
         }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.locationInWindow)
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.locationInWindow)
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.locationInWindow)
     }
     
     override func keyDown(with event: NSEvent) {
