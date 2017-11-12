05e371c5b2c6b26011952a6e8b1b541de7487d08
Milko Daskalov <mdaskalov@outlook.com>
2017-11-12 22:13:02 +0100

    Revert "Cosmetic"
    This reverts commit a204d1b724a104eebace8bdbc02d76953ab402cc.

--- MazePhone/GameScene.swift
+++ MazePhone/GameScene.swift
@@ -30,6 +30,9 @@ class GameScene: SKScene {
 
         let maze = MazeTileMapNode(columns: boxMapWidth, rows: boxMapHeight, boxSize: boxSize)
         
+        maze.position.x = 0
+        maze.position.y = 0
+        
         self.camera?.setScale(cameraScale)
         self.addChild(maze)
         self.maze = maze
